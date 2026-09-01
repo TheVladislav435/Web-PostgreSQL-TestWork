255fbe63473403a9687166846b772bed3bfc453e
Влад <skot.crau@yandex.ru>
2026-09-01 08:07:57 +0300

    Добавьте файлы проекта.

---
+++ Web-PostgreSQL-TestWork/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web_PostgreSQL_TestWork.Models;
+
+namespace Web_PostgreSQL_TestWork.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web-PostgreSQL-TestWork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_PostgreSQL_TestWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
